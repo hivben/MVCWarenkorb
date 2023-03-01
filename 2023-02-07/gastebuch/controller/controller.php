407f77496b40dd360f30f08426e50bddb26daa06
hivben <102210685+hivben@users.noreply.github.com>
2023-03-01 08:52:26 +0100

    Add files via upload

---
+++ 2023-02-07/gastebuch/controller/controller.php
@@ -0,0 +1,37 @@
+<?php
+    class Controller{
+
+        function __construct(){
+
+        }
+
+        function DBConnect_DatenEinlesen(){
+            if(!isset($_SESSION['dbLeser'])){
+                $_SESSION['dbLeser'] = new DBConnect();
+                $_SESSION['dbLeser']->DatenHolen("gaestebuch", "gastbuch");
+            }
+        }
+
+        function DBConnect_Datenhochladen($name, $email, $comment){
+            if(!isset($_SESSION['dbSchreiber'])){
+                $_SESSION['dbSchreiber'] = new DBConnect();
+                $_SESSION['dbSchreiber']->DatenHochladen("gaestebuch", "gastbuch", $name, $email, $comment);
+            }
+            else{
+                $_SESSION['dbSchreiber']->DatenHochladen("gaestebuch", "gastbuch", $name, $email, $comment);
+            }        
+        }
+        function Email_Verschicken($name, $email, $comment){
+            $to = $email;
+            $subject = $name;
+            $message = $comment;
+            
+            mail($to, $subject,$message);
+
+        }
+
+
+
+    }
+
+?>
---
+++ 2023-02-07/gastebuch/model/dbConnect.php
@@ -0,0 +1,46 @@
+﻿<?php
+    class DBConnect{
+
+
+        public $werte = array("0");
+
+        function __construkt(){
+           
+  
+        }
+
+        function DatenHolen($datenbank, $tabellenname){
+            $pdo = new PDO('mysql:host=localhost;dbname='.$datenbank, 'root', '');
+
+            $sql = "select * from $tabellenname";
+            $statement = $pdo->prepare($sql);
+		    $statement->execute();
+
+            $i = 0;
+            while($zeile = $statement->fetch())
+            {
+                
+                if($this->werte[0] == "0")
+                    $this->werte[0] = $zeile;
+                else
+                    array_push($this->werte, $zeile);
+                
+            }
+
+            $pdo = null;
+
+        }
+        public function DatenHochladen($datenbank, $tabellenname, $name, $email, $comment) {
+            $Datum = date('Y-m-d', time());
+            $pdo = new PDO('mysql:host=localhost;dbname='.$datenbank, 'root', '');
+
+            $statement = $pdo->prepare("INSERT INTO $tabellenname (Name, Email, Kommentar, Datum) VALUES (?,?,?,?)");
+            $statement->execute(array($name, $email, $comment, $Datum));
+
+            $pdo = null;
+        }
+
+
+    }
+
+?>
