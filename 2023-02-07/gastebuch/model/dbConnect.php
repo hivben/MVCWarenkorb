d01ba6b2745c02387d0e818cd3715d557cea8c91
hivben <102210685+hivben@users.noreply.github.com>
2023-03-01 09:29:22 +0100

    Add files via upload

---
+++ 2023-02-07/gastebuch/model/dbConnect.php
@@ -0,0 +1,50 @@
+﻿<?php
+    class DBConnect{
+
+
+        public $werte = array("0");
+
+        function __construkt(){
+           
+  
+        }
+
+        function DatenHolen($datenbank, $tabellenname){
+            $pdo = new PDO('mysql:host=localhost;dbname='.$datenbank, 'root', '');
+
+            $sql = "select * from $tabellenname";
+            $statement = $pdo->prepare($sql);
+		    $statement->execute();
+            echo "<table>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Kommentar</th>
+            </tr>";
+            
+            while ($zeile = $statement->fetch()) {
+                echo "<tr>
+                <th>".$zeile[1]."</th>
+                <th>".$zeile[2]."</th>
+                <th>".$zeile[3]."</th>
+                </tr>";
+            }
+            echo "</tabel>";
+
+            $pdo = null;
+
+        }
+        public function DatenHochladen($datenbank, $tabellenname, $name, $email, $comment) {
+            $Datum = date('Y-m-d', time());
+            $pdo = new PDO('mysql:host=localhost;dbname='.$datenbank, 'root', '');
+
+            $statement = $pdo->prepare("INSERT INTO $tabellenname (Name, Email, Kommentar, Datum) VALUES (?,?,?,?)");
+            $statement->execute(array($name, $email, $comment, $Datum));
+
+            $pdo = null;
+        }
+
+
+    }
+
+?>
---
+++ 2023-02-07/gastebuch/view/Template.php
@@ -0,0 +1,27 @@
+﻿<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+    </head>
+    <body>
+        <h1>Gästebuch</h1>
+
+        <h2>Neuen Eintrag hinzufügen</h2>
+        
+        <form action="index.php" method="post">
+            <label for="name">Name:</label>
+            <input type="text" name="name" id="name" required>
+
+            <label for="email">E-Mail:</label>
+            <input type="email" name="email" id="email" required>
+
+            <label for="comment">Kommentar:</label>
+            <textarea name="comment" id="comment" required></textarea>
+
+            <input type="submit" value="Eintrag absenden">
+        </form>
+
+        <h2>Alle Einträge</h2>
+        
+
+    </body>
+</html>
